1e9468f443025ede44f334cfb1b78c7c46584b5f
Khushi820-pixl <dautpurekhushi@gmail.com>
2026-07-03 15:14:42 +0530

    Add javaScript app.js

---
+++ javaScript app.js
@@ -0,0 +1,17 @@
+function addTask(){
+
+alert("Task Added Successfully");
+
+}
+
+function deleteTask(){
+
+alert("Task Deleted");
+
+}
+
+function updateStatus(){
+
+alert("Task Status Updated");
+
+}
